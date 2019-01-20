8797f1ba4ad3dd69bd277ba037d64eccd228c914
iampaul83 <iampaul783@gmail.com>
2019-01-20 10:59:14 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ['react'],
+  globals: {
+    graphql: false,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      ecmaVersion: 2018,
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+}
